d00f87511b87e15eadcf5fb9b57c965ae92fb4a8
Your Name <you@example.com>
2025-12-28 18:20:04 +0530

    02 - Datatypes of JavaScript

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all Js code as newer version
+
+// alert(3 + 33)
+
+/*
+1. String
+2. Number
+3. boolean
+4. bigint
+6. null -> When we give intentionaly
+7. undefined -> when variable is declared but not defined
+8. symbol -> to check the uniquess
+9. object[]
+
+we user typeof -> to check the type of the variable
+*/
+
+console.log(typeof null); // -> always return object as a type.
